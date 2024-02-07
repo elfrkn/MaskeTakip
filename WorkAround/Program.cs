8b0e289fde59fa44966cc5ac0397961a0d29ac39
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-02-07 22:54:16 +0100

    Add project files.

---
+++ WorkAround/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WorkAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Selamver();
+            Selamver();
+            Selamver();
+            int sonuc=Topla();
+            Console.ReadLine();
+
+        }
+
+        static void Selamver()
+        {
+            Console.WriteLine("Merhaba");
+        }
+
+        static int Topla()
+        {
+            return 5;
+        }
+    }
+}
+
+
