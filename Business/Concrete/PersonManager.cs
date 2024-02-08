15468f3e978faf89a5f5c1d0a6d2b2875389a3c3
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-02-08 11:35:42 +0100

    new changes

--- Business/Concrete/PersonManager.cs
+++ Business/Concrete/PersonManager.cs
@@ -4,14 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
 
 namespace Business.Concrete
 {
-    public class PersonManager
+    //Ciplak class kalmasin
+    public class PersonManager : IApplicantService
     {
+        //encapsulation
         public void ApplyForMask(Person person)
         {
 
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        //Ptt bu kisinin dogrulugunu(vatandas mi?) kontrol edecek.Mernise baglanacak.Kisinin dogrulugunu kontrol edecek.
+        public bool CheckPerson(Person person)
+        {
+
+            //Burada mernis kontrolu yapilacak.
+            return true;
         }
     }
 }
--- WorkAround/Program.cs
+++ WorkAround/Program.cs
@@ -6,22 +6,55 @@ namespace WorkAround
     {
         static void Main(string[] args)
         {
+            Selamver(isim:"Elif");
+            Selamver(isim:"Furkan");
+            Selamver(isim:"Selim");
+            Selamver(isim:"Hamza");
             Selamver();
-            Selamver();
-            Selamver();
-            int sonuc=Topla();
+            Topla(3,5);
+
+            //Array-Diziler
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Elif";
+            ogrenciler[1] = "Furkan";
+            ogrenciler[2] = "Selim";
+            ogrenciler = new string[4];
+            ogrenciler[3] = "Hamza";
+            for(int i=0; i<ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "Ankara", "Istanbul", "Izmir" };
+            string[] sehirler2 = new[] { "Bursa", "Bolu", "Sakarya" };
+            foreach(string sehir in sehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            List<string> yenisehirler1=new List<string> { "Adana", "Urfa", "Manisa" };
+            yenisehirler1.Add("Gaziantep");
+            foreach(string sehir in yenisehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+
             Console.ReadLine();
 
         }
 
-        static void Selamver()
+        static void Selamver(string isim="isimsiz")
         {
-            Console.WriteLine("Merhaba");
+            Console.WriteLine("Merhaba" + isim);
         }
 
-        static int Topla()
+        static int Topla(int sayi1,int sayi2)
         {
-            return 5;
+            int sonuc= sayi1 + sayi2;
+            Console.WriteLine("toplam= " + sonuc);
+            return sonuc;
         }
     }
 }
